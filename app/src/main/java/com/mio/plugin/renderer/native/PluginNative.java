b38892741cb82fb806a488846be6785d6c988006
denotaravys <denotaravys@gmail.com>
2026-04-27 21:38:23 +0200

    Corrigir PluginNative.java e remover libxx.so placeholder

--- app/src/main/java/com/mio/plugin/renderer/native/PluginNative.java
+++ app/src/main/java/com/mio/plugin/renderer/native/PluginNative.java
@@ -4,7 +4,8 @@ public class PluginNative {
     static {
         try {
             System.loadLibrary("maliplugin");
-        } catch (UnsatisfiedLinkError ignored) {}
+        } catch (UnsatisfiedLinkError ignored) {
+        }
     }
 
     public static native String getGLExtensions();
